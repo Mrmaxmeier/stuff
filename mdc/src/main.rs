714a7a8b909ad316817e4c5b09f3ecb473272843
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-05-18 21:55:42 +0200

    implement `mdc queue <uri>`

--- mdc/src/main.rs
+++ mdc/src/main.rs
@@ -3,6 +3,11 @@
 
 extern crate rustc_serialize;
 extern crate docopt;
+extern crate hyper;
+extern crate url;
+
+use hyper::client::Client;
+use url::Url;
 
 docopt!(Args derive Debug, "
 mediad-client.
@@ -18,13 +23,23 @@ Options:
   -h --help     Show this screen.
   -V --version  Show version.
   -v --verbose  Display verbose logs.
-  --port        Server port.
+  --port=<kn>   Server port. [default: 9922]
 ");
-// [default: 7227]
+// FIXME set port [default: 9922]
+
 
 fn main() {
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
-    println!("{}", args.flag_verbose);
-    println!("{}", args.arg_uri);
+    let port = 9922; //args.arg_port.or_else(9922);
+    if args.cmd_queue {
+        println!("enqueueing '{}'...", args.arg_uri);
+        let client = Client::new();
+        let mut url = Url::parse("http://localhost/enqueue").unwrap();
+        url.query_pairs_mut().append_pair("uri", &*args.arg_uri);
+        let _ = url.set_port(Some(port));
+        client.post(url)
+            .send()
+            .unwrap();
+    }
 }
