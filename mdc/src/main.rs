c7183f03bd4bb4a1d95c515c7658331058558992
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-30 21:45:57 +0200

    implement in-place restart

--- mdc/src/main.rs
+++ mdc/src/main.rs
@@ -14,6 +14,8 @@ use hyper::status::StatusCode;
 use url::Url;
 use std::io::prelude::*;
 use std::path::Path;
+use std::time::Duration;
+use std::thread::sleep;
 
 docopt!(Args derive Debug, "
 mediad-client.
@@ -74,6 +76,23 @@ fn mpv_cmd(url_base: &Url, args: Vec<&str>) -> String {
     let mut s = String::new();
     response.read_to_string(&mut s).unwrap();
     s
+}
+
+fn mpv_cmd_nr(url_base: &Url, args: Vec<&str>) -> Result<(), ()> {
+    let mut url = url_base.clone();
+    url.set_path("command");
+    {
+        let mut query_pairs = url.query_pairs_mut();
+        for arg in args {
+            query_pairs.append_pair("arg", arg);
+        }
+    }
+    url.query_pairs_mut().append_pair("no-wait", "true");
+    let mut response = send(url);
+    // println!("{:?}", response);
+    let mut s = String::new();
+    response.read_to_string(&mut s).unwrap();
+    Ok(())
 }
 
 fn valid_file_path(path_str: &str) -> Option<String> {
@@ -169,7 +188,33 @@ fn main() {
             }
         }
         Commands::Restart => {
-            unimplemented!();
+            let playlist_data = mpv_cmd(&url, vec!["get_property", "playlist"]);
+            let playlist: Vec<PlaylistEntry> = serde_json::from_str(&playlist_data).unwrap();
+            let time: f64 = mpv_cmd(&url, vec!["get_property", "time-pos"]).parse().unwrap();
+            println!("playlist: {:#?}", playlist);
+            println!("time current: {}", time);
+            println!("quitting mpv...");
+            mpv_cmd_nr(&url, vec!["quit"]).unwrap();
+            sleep(Duration::from_secs(1));
+            let mut found_current = false;
+            for entry in &playlist {
+                let is_current = entry.current.unwrap_or(false);
+                found_current |= is_current;
+
+                if found_current {
+                    println!("queueing {:?}", entry.filename);
+                    mpv_cmd(&url, vec!["loadfile", &*entry.filename, "append-play"]);
+                    sleep(Duration::from_secs(2));
+                }
+
+                if is_current {
+                    println!("waiting for file load...");
+                    sleep(Duration::from_secs(5)); // TODO wait for actual events
+                    println!("seeking to {:?}", time);
+                    mpv_cmd(&url, vec!["seek", &*format!("{}", time), "absolute+keyframes"]);
+                    sleep(Duration::from_secs(2));
+                }
+            }
         }
     }
 }
