d784ed8feee6fc0166292877893a61717c18a95e
Mrmaxmeier <Mrmaxmeier@gmail.com>
2023-02-25 20:07:55 +0100

    kernel-version-diff: fix edge-case

--- kernel-version-diff.sh
+++ kernel-version-diff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-FILTER="[0-9]\+\.[0-9]\+\.[0-9]\+"
-RUNNING=$(uname -r | grep -o "$FILTER")
-INSTALLED=$(pacman -Qi linux | grep -i "version" | grep -o "$FILTER")
+FILTER='[0-9]+\.[0-9]+(\.[0-9]+)?'
+RUNNING=$(uname -r | rg -o "$FILTER")
+INSTALLED=$(pacman -Qi linux | grep -i "version" | rg -o "$FILTER")
 
 echo "  RUNNING: $RUNNING"
 echo "INSTALLED: $INSTALLED"
